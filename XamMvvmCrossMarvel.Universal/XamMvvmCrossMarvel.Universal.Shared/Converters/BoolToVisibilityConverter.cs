8926fb01330c7a75ab606e16e5585685878c7f73
Mario López Baratas <mariolopez000@hotmail.com>
2016-01-08 19:35:28 +0100

    New projects for windows universal. Windows store project working except back button

---
+++ XamMvvmCrossMarvel.Universal/XamMvvmCrossMarvel.Universal.Shared/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace XamMvvmCrossMarvel.Universal.Converters
+{
+    class BoolToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// If set to True, conversion is reversed: True will become Collapsed.
+        /// </summary>
+        public bool IsReversed { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var val = System.Convert.ToBoolean(value);
+            if (this.IsReversed)
+            {
+                val = !val;
+            }
+
+            if (val)
+            {
+                return Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
